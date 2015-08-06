3a8137e5933f65a2f5dfeda62ab1ff9b01ec4aac
Alexey Kuzevanov <leshiy1000.kuz@yandex.ru>
2015-08-06 13:00:55 +0600

    fixed bug

--- src/cos.h
+++ src/cos.h
@@ -2,13 +2,14 @@
 #define COS_H
 
 #include <math.h>
+#include <stdio.h>
 
 void my_cos(void)
 {
 	double arg;
 
-	scanf("%f", &arg);
-	printf("cos(%f) = %f\n", arg, cos(arg));
+	scanf("%lf", &arg);
+	printf("cos(%lf) = %lf\n", arg, cos(arg));
 }
 
 #endif
