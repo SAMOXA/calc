98ccfeb1259f6fd9924b38edb99594be65840da1
Alexey Kuzevanov <leshiy1000.kuz@yandex.ru>
2015-08-06 14:12:08 +0600

    linal version of function cos

--- src/cos.h
+++ src/cos.h
@@ -3,13 +3,22 @@
 
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define BUF_SIZE 16
 
 void my_cos(void)
 {
 	double arg;
+	char str[BUF_SIZE] = {0};
 
-	scanf("%lf", &arg);
-	printf("cos(%lf) = %lf\n", arg, cos(arg));
+	printf("enter argument of the function cos: ");
+	fgets(str, BUF_SIZE, stdin);
+	arg = atof(str);
+	if (arg <= 1 && arg >= -1)
+		printf("cos(%lf) = %lf\n", arg, cos(arg));
+	else
+		printf("error: argument not belong interval [-1;1]\n");
 }
 
 #endif
