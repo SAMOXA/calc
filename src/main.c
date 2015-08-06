f78a9bed38b2a99666819729275eaa81da5ee0ba
SAMOXA <amdsamoxa@gmail.com>
2015-08-06 13:45:29 +0600

    Test cos and mul

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include "multiplication.h"
+#include "cos.h"
 
 int main() {
-	printf("TODO calc\n");
+	my_cos();
+	multiplication();
+	printf("1) Add\n");
+	printf("2) Sub\n");
+	printf("3) Mul\n");
+	printf("4) Div\n");
+	printf("5) \n");
+	printf("6) Add\n");
+	printf("7) Add\n");
+	printf("Action: ");
 	return 0;
 }
